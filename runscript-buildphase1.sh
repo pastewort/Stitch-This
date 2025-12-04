911491fad0b9fe67be664dc9aa68175cf41fe2d8
Martin Faulks <martin.faulks@ntlworld.com>
2025-12-04 13:54:13 +0000

    Delete runscript-buildphase1.sh

--- runscript-buildphase1.sh
+++
@@ -1,5 +0,0 @@
-export DYLIB=libfreeimage.3.dylib
-
-cp -f "$SRCROOT/$DYLIB" "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/Frameworks"
-
-install_name_tool -change @executable_path/$DYLIB @loader_path/../Frameworks/$DYLIB "$TARGET_BUILD_DIR/$TARGET_NAME.app/Contents/MacOS/$PRODUCT_NAME"
